5f82fc23442d5b8f962032ad56524358d739a452
khalil <khalilelqaddabi9@gmail.com>
2025-12-27 10:55:04 +0100

    transfer

---
+++ classes/patient.php
@@ -0,0 +1,21 @@
+<?php
+
+use Dba\Connection;
+
+include "/classes/person.php";
+require_once "/config.php";
+
+
+class Patient extends Person { 
+
+    public $age;
+    public $gender;
+    public $adress;
+    public $Connection;
+    public function __construct(
+
+    ) {
+
+
+}
+}
--- config.php
+++ config.php
@@ -1,16 +1,14 @@
 <?php
 class DAtabase {
 
-    private $host = '??????????';
-    private $dbname = '??????????';
-    private $username = '??????????
-';
-    private $password = '??????????
-';
+    private $host = 'localhost';
+    private $dbname = 'backend v1 unity care clinic';
+    private $username = 'root';
+    private $password = '';
     private $charset = 'utf8mb4';
     private $conn;
     
-    function connect() {
+    public function connect() {
 
         try {
             $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname;charset=$this->charset", $this->username, $this->password);
